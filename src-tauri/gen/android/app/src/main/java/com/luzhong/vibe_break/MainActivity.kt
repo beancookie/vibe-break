2c49c6d0a09244547b5e3a5ac03548c5ad5768a1
LuZhong <luzhong256@gmail.com>
2026-06-30 22:59:02 +0800

    feat: add Android platform support via tauri android init, switch reqwest to rustls for cross-compilation

---
+++ src-tauri/gen/android/app/src/main/java/com/luzhong/vibe_break/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.luzhong.vibe_break
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
